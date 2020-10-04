77a6d9fc53756697d9f176a226a6a3262e8d9904
Finc8888 <finc88@gmail.com>
2020-10-04 20:25:09 +0300

    add Task class

---
+++ src/task.ts
@@ -0,0 +1,13 @@
+class Task {
+    defaultTaskList: string[];
+    constructor() {
+        this.defaultTaskList = [
+            'Напилить дров',
+            'Побелка в зале',
+            'Прибраться где были уточки',
+            'Покосить',
+            'Заштопать карманы'
+        ]
+    }
+}
+export default Task;

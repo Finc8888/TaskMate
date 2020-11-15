15de2717a118daa8cea3a324a4e84810e045dafa
Finc8888 <finc88@gmail.com>
2020-11-15 20:16:28 +0300

    fix(main):adding jQuert in index.html 1)Функция вывода строки текущего времени

--- static/index.ts
+++ static/index.ts
@@ -8,6 +8,18 @@ const UrlClearTasks = '/truncate_tasks';
 const UrlRemoveTask = '/remove_task'
 const UrlCreateTask = '/create_task'
 
+async function getTimeNow(){
+    setInterval(()=>{
+        const date = new Date;
+        const hours = date.getHours();
+        const min = date.getMinutes();
+        const sec = date.getSeconds();
+        const time = `${hours}:${min}:${sec}`;
+        console.log(time);
+        return time;
+        },1000);
+}
+
 /**
  * Обработчик создания новой задачи
  */
@@ -34,12 +46,13 @@ const buildTasks = async ( ) => {
     task.taskList = await task.getTasks(UrlTasks);
     // очистка списка задач на странице
     let listElement = document.getElementsByClassName('task-list')[0];
-    while (listElement.firstChild) {
-        listElement.removeChild(listElement.firstChild);
-    }
-    for(let taskObj of task.taskList){
+    if(listElement){
+        while (listElement.firstChild) {
+            listElement.removeChild(listElement.firstChild);
+        }
+        for(let taskObj of task.taskList){
 
-        let item = `
+            let item = `
         <div class="card-group">
             <div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
                 <div class="card-header">
@@ -53,22 +66,26 @@ const buildTasks = async ( ) => {
             </div>
         </div>
         `;
-        let listElement = document.getElementsByClassName('task-list')[0];
-        listElement?.insertAdjacentHTML('beforeend',item);
-        // на каждую кнопку вешаем свой обработчик удаления задачи
-        document.getElementById(`btn-delete-task_${taskObj.id}`)?.addEventListener('click',async (e:any) =>{
-            const id = e.target.dataset.id;
-            await fetch(`${UrlRemoveTask}/${id}`, {method:'delete',body:''});
-            await buildTasks();
-            e.preventDefault();
-        });
+            let listElement = document.getElementsByClassName('task-list')[0];
+            listElement?.insertAdjacentHTML('beforeend',item);
+            // на каждую кнопку вешаем свой обработчик удаления задачи
+            document.getElementById(`btn-delete-task_${taskObj.id}`)?.addEventListener('click',async (e:any) =>{
+                const id = e.target.dataset.id;
+                await fetch(`${UrlRemoveTask}/${id}`, {method:'delete',body:''});
+                await buildTasks();
+                e.preventDefault();
+            });
+        }
     }
+
 }
 /**
  * Запуск текущего функционала приложения
  */
 const run = () => {
     buildTasks();
+
+    // getTimeNow();
 
 }
 
